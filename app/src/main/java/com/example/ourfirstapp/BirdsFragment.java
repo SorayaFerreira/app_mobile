3a01644dd50c164691b9d16d5badb76f65883b9d
João Pedro Teixeira do Espirito Santo <txrajoao@gmail.com>
2025-04-17 11:04:25 -0400

    "Feature - navegação inicial com botoes de teste"

---
+++ app/src/main/java/com/example/ourfirstapp/BirdsFragment.java
@@ -0,0 +1,78 @@
+package com.example.ourfirstapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BirdsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BirdsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public BirdsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BirdsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BirdsFragment newInstance(String param1, String param2) {
+        BirdsFragment fragment = new BirdsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_birds, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //navegar para a pagina de detalhes da ave atraves de um botao de teste
+        Button button = view.findViewById(R.id.arara);
+        button.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_birdsFragment_to_birdDetailsFragment);
+        });
+    }
+}
+
--- app/src/main/java/com/example/ourfirstapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ourfirstapp/ui/home/HomeFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.ourfirstapp.R;
 import com.example.ourfirstapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -27,8 +31,19 @@ public class HomeFragment extends Fragment {
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //navegar para a pagina de aves atraves de um botao de teste
+        Button button = view.findViewById(R.id.aves);
+        button.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_navigation_home_to_birdsFragment);
+        });
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
